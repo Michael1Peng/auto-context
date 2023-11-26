7e1f59813ab0378e617809d54d1fcdb825f34737
Michael Peng <michael1peng@outlook.com>
2023-11-26 22:07:57 +0800

    feat: Refactor insertContentFromAllTabs function to insert content from all open documents as comments

--- src/insertContentFromAllTabs.ts
+++ src/insertContentFromAllTabs.ts
@@ -4,17 +4,40 @@ export function insertContentFromAllTabs() {
     const allOpenDocuments = vscode.workspace.textDocuments;
     const activeEditor = vscode.window.activeTextEditor;
 
-    if (activeEditor) {
-        allOpenDocuments.forEach(document => {
-            if (document.uri.scheme!=='file') {
-                return;
-            }
-
-            const content = document.getText();
-            activeEditor.edit(editBuilder => {
-                const startOfDocument = activeEditor.document.positionAt(0);
-                editBuilder.insert(startOfDocument, content + "\n"); // 添加换行符进行分隔
-            });
-        });
+    if (!activeEditor) {
+        return;
     }
+
+    const activeDocumentUri = activeEditor.document.uri.toString();
+
+    allOpenDocuments.forEach(document => {
+        if (document.uri.scheme !== 'file' || document.uri.toString() === activeDocumentUri) {
+            return;
+        }
+
+        const content = document.getText();
+        const formattedContent = formatContentAsComments(content, activeEditor.document.languageId);
+
+        activeEditor.edit(editBuilder => {
+            const startOfDocument = activeEditor.document.positionAt(0);
+            editBuilder.insert(startOfDocument, formattedContent + "\n\n");
+        });
+    });
+}
+
+function formatContentAsComments(content: string, languageId: string): string {
+    const commentSyntax = getCommentSyntaxForLanguage(languageId);
+    return content.split('\n').map(line => commentSyntax + line).join('\n');
+}
+
+function getCommentSyntaxForLanguage(languageId: string): string {
+    // Map of language IDs to their respective comment syntax
+    const commentSyntaxMap: { [id: string]: string } = {
+        'javascript': '// ',
+        'typescript': '// ',
+        'python': '# ',
+        // Add more languages as needed
+    };
+
+    return commentSyntaxMap[languageId] || '// '; // Default to '//' if language is not in the map
 }
