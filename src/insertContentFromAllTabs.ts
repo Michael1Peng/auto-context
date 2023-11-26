0c4bf886de26c465b677d3e598f15e21b778296d
Michael Peng <michael1peng@outlook.com>
2023-11-26 22:20:33 +0800

    feat: Refactor insertContentFromAllTabs function

--- src/insertContentFromAllTabs.ts
+++ src/insertContentFromAllTabs.ts
@@ -1,3 +1,5 @@
+// src/insertContentFromAllTabs.ts
+
 import * as vscode from 'vscode';
 
 export function insertContentFromAllTabs() {
@@ -9,14 +11,17 @@ export function insertContentFromAllTabs() {
     }
 
     const activeDocumentUri = activeEditor.document.uri.toString();
+    const existingCommentedFiles = parseExistingComments(activeEditor.document);
 
     allOpenDocuments.forEach(document => {
-        if (document.uri.scheme !== 'file' || document.uri.toString() === activeDocumentUri) {
+        if (document.uri.scheme !== 'file' || document.uri.toString() === activeDocumentUri || existingCommentedFiles.includes(document.uri.toString())) {
             return;
         }
 
-        const content = document.getText();
-        const formattedContent = formatContentAsComments(content, activeEditor.document.languageId);
+        let content = document.getText();
+        content = filterOutExistingComments(content);
+
+        const formattedContent = formatContentAsComments(content, document.uri.toString());
 
         activeEditor.edit(editBuilder => {
             const startOfDocument = activeEditor.document.positionAt(0);
@@ -25,19 +30,24 @@ export function insertContentFromAllTabs() {
     });
 }
 
-function formatContentAsComments(content: string, languageId: string): string {
-    const commentSyntax = getCommentSyntaxForLanguage(languageId);
-    return content.split('\n').map(line => commentSyntax + line).join('\n');
+function filterOutExistingComments(content: string): string {
+    const commentBlockRegex = /\/\/ CHUNK START[\s\S]*?\/\/ CHUNK END\n/g;
+    return content.replace(commentBlockRegex, '');
 }
 
-function getCommentSyntaxForLanguage(languageId: string): string {
-    // Map of language IDs to their respective comment syntax
-    const commentSyntaxMap: { [id: string]: string } = {
-        'javascript': '// ',
-        'typescript': '// ',
-        'python': '# ',
-        // Add more languages as needed
-    };
+function formatContentAsComments(content: string, fileUri: string): string {
+    return `// CHUNK START\n// file: ${fileUri}\n${content.split('\n').map(line => '// ' + line).join('\n')}\n// CHUNK END`;
+}
 
-    return commentSyntaxMap[languageId] || '// '; // Default to '//' if language is not in the map
+function parseExistingComments(document: vscode.TextDocument): string[] {
+    const text = document.getText(new vscode.Range(new vscode.Position(0, 0), new vscode.Position(10, 0))); // Read the first 10 lines
+    const regex = /\/\/ file: (.+)/g;
+    const matchedFiles = [];
+    let match;
+
+    while ((match = regex.exec(text)) !== null) {
+        matchedFiles.push(match[1]);
+    }
+
+    return matchedFiles;
 }
