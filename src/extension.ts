452cdc5c1a2ab94750470f2acdcfc82274d2247e
Michael1Peng <pengchangping.michael9@bytedance.com>
2023-11-29 11:56:48 +0800

    feat: 切换tab 的时候去除顶部context 注释

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,10 @@ import * as vscode from 'vscode';
 
 import { insertContentFromAllTabs } from './insertContentFromAllTabs';
 import { OutputChannelManager } from './outputChannelManager';
+import { removeTopCommentBlocks } from './removeTopCommentBlocks';
 
 let isEdited = false;
+let lastActiveEditor: vscode.TextEditor | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
 	OutputChannelManager.initialize("auto-context");
@@ -11,6 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let resetEditMarkListener = vscode.window.onDidChangeActiveTextEditor(() => {
 		isEdited = false;
+
+		if (lastActiveEditor && lastActiveEditor !== vscode.window.activeTextEditor) {
+			removeTopCommentBlocks(lastActiveEditor);
+		}
+		lastActiveEditor = vscode.window.activeTextEditor;
 	});
 	context.subscriptions.push(resetEditMarkListener);
 
---
+++ src/removeTopCommentBlocks.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+
+export const commentBlockRegex = /\/\/ CHUNK START[\s\S]*?\/\/ CHUNK END\n\n/g;
+
+export function removeTopCommentBlocks(editor: vscode.TextEditor) {
+  const document = editor.document;
+  const fileUri = document.uri;
+
+  // 读取文件内容
+  vscode.workspace.fs.readFile(fileUri).then((buffer) => {
+    let content = buffer.toString();
+
+    // 移除特定格式的注释块
+    content = content.replace(commentBlockRegex, '');
+
+    // 写回文件
+    const writeData = Buffer.from(content, 'utf8');
+    vscode.workspace.fs.writeFile(fileUri, writeData);
+  });
+}
