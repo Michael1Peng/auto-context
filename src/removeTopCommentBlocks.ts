a27ba94c8489ca564f29a03d297ea2aba9d32ebe
Michael1Peng <pengchangping.michael9@bytedance.com>
2023-12-13 14:45:28 +0800

    feat: 关闭文件时清理copilot 标记

--- src/removeTopCommentBlocks.ts
+++ src/removeTopCommentBlocks.ts
@@ -1,6 +1,11 @@
 import * as vscode from 'vscode';
 
 export const commentBlockRegex = /\/\/ CHUNK START[\s\S]*?\/\/ CHUNK END\n\n/g;
+const copilotContextStartRegex = /\/\/ \[COPILOT CONTEXT\] Start\n/;
+const copilotContextEndRegex = /\/\/ \[COPILOT CONTEXT\] End here\n/;
+
+let config = vscode.workspace.getConfiguration('auto-context');
+let cleanCopilotContextCommentOnClose = config.get('cleanCopilotContextCommentOnClose');
 
 export function removeTopCommentBlocks(document: vscode.TextDocument) {
   const fileUri = document.uri;
@@ -11,6 +16,10 @@ export function removeTopCommentBlocks(document: vscode.TextDocument) {
 
     // 移除特定格式的注释块
     content = content.replace(commentBlockRegex, '');
+    if (cleanCopilotContextCommentOnClose) {
+      content = content.replace(copilotContextStartRegex, '');
+      content = content.replace(copilotContextEndRegex, '');
+    }
 
     // 写回文件
     const writeData = Buffer.from(content, 'utf8');
